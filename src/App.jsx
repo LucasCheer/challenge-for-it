4a7d7aa6be7a65eb820ea5c06514ec1ba34d41a1
Lucas <cheroyonatan2@gmail.com>
2025-02-07 23:21:16 -0300

    Add filter search

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import UserCard from './components/Usercard.jsx'
 
@@ -7,34 +7,46 @@ function App() {
   const URL = 'https://jsonplaceholder.typicode.com/users'
 
   const [users, setUsers] = useState([])
+  const [search, setSearch] = useState('')
 
-  
+  const filterUsers = users.filter(user => user.name.toLowerCase().includes(search.toLowerCase()))
+
   const fetchUsers = async () => {
 
-    try{
+    try {
       const response = await fetch(URL)
       const data = await response.json()
       setUsers(data)
     }
-    catch(error){
+    catch (error) {
       console.log(error)
     }
   }
-  
+
   useEffect(() => { fetchUsers() }, [])
-  
+
   return (
     <>
-    <header className='header'>
-      <h1>Challenge For It</h1>
-    </header>
-    <main className='main'>
-      {
-        users.map((user) => (
-          <UserCard key={user.id} user={user} />
-        ))
-      }
-    </main>
+      <header className='header'>
+        <h1>Challenge For It</h1>
+        <input
+          type="text"
+          placeholder='Buscar por nombre'
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          className='search-input'
+        />
+      </header>
+      <main className='main'>
+        {filterUsers.length > 0 ? (
+          filterUsers.map((user) => (
+            <UserCard key={user.id} user={user} />
+          ))
+        ) : (
+          <p>No se encontraron resultados</p>
+        )}
+      </main>
+
     </>
   )
 }
