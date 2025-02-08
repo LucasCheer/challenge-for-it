28a7bd4493453fd1a12170f79a7c70b54c9aaab2
Lucas <cheroyonatan2@gmail.com>
2025-02-07 21:51:13 -0300

    Fetch Function and Cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,40 @@
-import { useState } from 'react'
+import {useState, useEffect } from 'react'
 import './App.css'
+import Usercard from './components/Usercard.jsx'
 
 function App() {
+
+  const URL = 'https://jsonplaceholder.typicode.com/users'
+
+  const [users, setUsers] = useState([])
+
+  
+  const fetchUsers = async () => {
+
+    try{
+      const response = await fetch(URL)
+      const data = await response.json()
+      setUsers(data)
+    }
+    catch(error){
+      console.log(error)
+    }
+  }
+  
+  useEffect(() => { fetchUsers() }, [])
   
   return (
     <>
+    <header className='header'>
+      <h1>Challenge For It</h1>
+    </header>
+    <main className='main'>
+      {
+        users.map((user) => (
+          <Usercard key={user.id} user={user} />
+        ))
+      }
+    </main>
     </>
   )
 }
---
+++ src/components/Usercard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Usercard({ user }) {
+
+    return (
+        <div className='card'>
+            <h2>{user.name}</h2>
+            <p><strong>Username:</strong> {user.username}</p>
+            <p><strong>Email:</strong> {user.email}</p>
+            <p><strong>Ciudad:</strong> {user.address.city}</p>
+            <p><strong>Teléfono:</strong> {user.phone}</p>
+            <p><strong>Empresa:</strong> {user.company.name}</p>
+        </div>
+    )
+}
